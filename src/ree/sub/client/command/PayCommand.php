d44282e4c4c2f3154a4cad0f32ecea97c96b3628
Ree_ <ree.dev.jp@gmail.com>
2020-02-04 01:14:00 +0900

    ギュッと小さくしたいお年頃だった

--- src/ree/sub/client/command/PayCommand.php
+++ src/ree/sub/client/command/PayCommand.php
@@ -43,24 +43,14 @@ class PayCommand extends Command
 						if ($receive instanceof Player) {
 							$sender->sendMessage(ReefCoinClient::GOOD . TextFormat::GREEN . $args[0] . TextFormat::RESET . 'さんに' . $args[1] . 'coin送金しました');
 							$receive->sendMessage(ReefCoinClient::GOOD . TextFormat::GREEN . $sender->getName() . TextFormat::RESET . 'さんから' . $args[1] . 'coin送金されました');
-						} else {
-							$sender->sendMessage(ReefCoinClient::GOOD . TextFormat::GRAY . $args[0] . TextFormat::RESET . 'さんに' . $args[1] . 'coin送金しました');
-						}
-					} else {
-						$sender->sendMessage(ReefCoinClient::ERROR . 'Unknown error');
-					}
-				} else {
-					$sender->sendForm(new ReefCoinPayForm('お金が足りません', $args[0]));
-				}
-			} else {
-				$sender->sendForm(new ReefCoinPayForm('見つかりませんでした', $args[0]));
-			}
+						} else $sender->sendMessage(ReefCoinClient::GOOD . TextFormat::GRAY . $args[0] . TextFormat::RESET . 'さんに' . $args[1] . 'coin送金しました');
+					} else $sender->sendMessage(ReefCoinClient::ERROR . 'Unknown error');
+				} else $sender->sendForm(new ReefCoinPayForm('お金が足りません', $args[0]));
+			} else $sender->sendForm(new ReefCoinPayForm('見つかりませんでした', $args[0]));
 		} else {
 			if (empty($args)) {
 				$sender->sendForm(new ReefCoinPayForm());
-			} else {
-				$sender->sendForm(new ReefCoinPayForm('コマンドが間違っています'));
-			}
+			} else $sender->sendForm(new ReefCoinPayForm('コマンドが間違っています'));
 		}
 		return true;
 	}
