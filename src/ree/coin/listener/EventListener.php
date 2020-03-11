d89ea11e08d350069f37fa2971e15843aae64bf0
Ree_ <ree.dev.jp@gmail.com>
2020-03-11 21:21:53 +0900

    add  setChangeDefaultCoin, getChangeDefaultCoin

--- src/ree/coin/listener/EventListener.php
+++ src/ree/coin/listener/EventListener.php
@@ -4,7 +4,7 @@
 namespace ree\coin\listener;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerLoginEvent;
 use ree\coin\ReefCoinAPI;
 use ree\coin\ReefCoinSystemCore;
 
@@ -13,16 +13,26 @@ class EventListener implements Listener
 	/**
 	 * @var int
 	 */
-	public $newAccountMoney = 9999999999;
+	private static $defaultCoin = 0;
 
-	public function onJoin(PlayerJoinEvent $ev)
+	public function onLogin(PlayerLoginEvent $ev)
 	{
 		$p = $ev->getPlayer();
 		$n = $p->getName();
 		if (!ReefCoinAPI::isExists($n))
 		{
-			ReefCoinAPI::setCoin($n ,$this->newAccountMoney);
-			$p->sendMessage(ReefCoinSystemCore::GOOD.'アカウントを作成しました');
+			ReefCoinAPI::setCoin($n ,self::$defaultCoin);
+			$p->sendMessage(ReefCoinSystemCore::GOOD.'create new account');
 		}
+	}
+
+	public static function setDefaultCoin(int $coin): void
+	{
+		self::$defaultCoin = $coin;
+	}
+
+	public static function getDefaultCoin(): int
+	{
+		return self::$defaultCoin;
 	}
 }
